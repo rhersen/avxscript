ce46d761ac14f1980f83128f3a8f1c55c922ec7f
Rudolf Hersén <rudolf@qwaya.com>
2015-04-01 07:48:03 +0200

    substitutes one parameter

--- s.js
+++ s.js
@@ -4,11 +4,16 @@ var globl = _.template('.globl _${ name }');
 var label = _.template('_${ name }:');
 
 module.exports = function (lines) {
-    var declaration = /double (\w+)/.exec(_.first(lines));
+    var declaration = /double (\w+)\(double (\w+)\)/.exec(_.first(lines));
     var options = {name: declaration[1]};
+
     return [
         '.intel_syntax noprefix',
         globl(options),
         label(options)
-    ].concat(_.rest(lines))
+    ].concat(_.map(_.rest(lines), substitute))
+
+    function substitute(line) {
+        return line.replace(new RegExp(declaration[2], 'g'), 'xmm0')
+    }
 }
--- test/test.js
+++ test/test.js
@@ -1,7 +1,7 @@
 var assert = require("assert")
 var s = require("../s")
 describe('s', function () {
-    it('prepends intel_syntax and globl', function () {
+    it('prepends intel_syntax and globl and substitutes xmm0', function () {
         assert.deepEqual(
             [
                 '.intel_syntax noprefix',
@@ -11,8 +11,25 @@ describe('s', function () {
                 'ret'
             ],
             s([
-                'double f()',
+                'double f(double x)',
+                'sqrtsd x, x',
+                'ret'
+            ])
+        )
+    })
+
+    it('substitutes named parameter', function () {
+        assert.deepEqual(
+            [
+                '.intel_syntax noprefix',
+                '.globl _f',
+                '_f:',
                 'sqrtsd xmm0, xmm0',
+                'ret'
+            ],
+            s([
+                'double f(double y)',
+                'sqrtsd y,, y',
                 'ret'
             ])
         )
